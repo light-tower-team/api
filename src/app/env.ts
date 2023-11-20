cb21772361da4cec964243081bf8f7e5643bcac9
Sherbakov Mikhail <michailshcherbakov@gmail.com>
2023-11-20 17:43:14 +0300

    fix: change env config

--- src/app/env.ts
+++ src/app/env.ts
@@ -1,10 +1,10 @@
 import { z } from "zod";
 
 const ENV_SCHEMA = z.object({
-  DATABASE_URL: z.string(),
-  PORT: z.string().transform(Number).pipe(z.number().positive()),
-  HOST: z.string(),
-  JWT_SECRET: z.string(),
+  DATABASE_URL: z.string().trim().min(1),
+  PORT: z.string().trim().transform(Number).pipe(z.number().positive()),
+  HOST: z.string().trim().min(1),
+  JWT_SECRET: z.string().trim().min(1),
 });
 
 export const env = ENV_SCHEMA.parse(process.env);
